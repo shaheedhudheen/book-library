6ccda5d4a3ab249f68747394e7b9c30d9afca098
Ahammed Shaheedhudheen <shaheedhudheenpambalath@gmail.com>
2022-09-18 10:27:20 +0530

    added add book pop-up

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 //select elements
-const bookGrid = document.querySelector('.books-grid')
-const book = document.querySelector('.book')
-const title = document.querySelector('.title')
-const author = document.querySelector('.author')
-const page = document.querySelector('.page')
-const completed = document.querySelector('.completed')
+const bookGrid = document.querySelector(".books-grid");
+const book = document.querySelector(".book");
+const title = document.querySelector(".title");
+const author = document.querySelector(".author");
+const page = document.querySelector(".page");
+const read = document.querySelector(".read");
 
-const addBookButton = document.querySelector('.button')
+const addBookButton = document.querySelector(".addBookButton");
+const popup = document.querySelector(".popup-wrapper");
+const close = document.querySelector(".close");
 //library array
 let myLibrary = [];
 
@@ -18,20 +20,17 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-
 myLibrary.push(new Book("the hobbit", "anoos", 232, true));
 myLibrary.push(new Book("the lord of the rings", "saif", 323, false));
 myLibrary.push(new Book("harry potter", "athul", 354, false));
 myLibrary.push(new Book("cucek", "abrar", 534, true));
 myLibrary.push(new Book("castle black", "aslam", 234, true));
 
-
 Book.prototype.info = function () {
   return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
 };
 
 let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 234, "not read yet");
-
 
 //add book to library function
 function addBookToLibrary() {
@@ -43,34 +42,42 @@ function addBookToLibrary() {
   myLibrary.push(new Book(title, author, pages, read));
 }
 
-
 // display every books added from library array
-myLibrary.forEach(book => {
-    let bookCard = document.createElement('div');
-    console.log(book);
-    bookGrid.appendChild(bookCard);
-    bookCard.classList.add("book");
-    
-    let booktitle = document.createElement('p');
-    booktitle.textContent = book.title;
-    booktitle.classList.add("title");
-    bookCard.appendChild(booktitle)
+myLibrary.forEach((book) => {
+  let bookCard = document.createElement("div");
+  console.log(book);
+  bookGrid.appendChild(bookCard);
+  bookCard.classList.add("book");
 
-    let bookAuthor = document.createElement('p');
-    bookAuthor.textContent = book.author;
-    bookAuthor.classList.add("author");
-    bookCard.appendChild(bookAuthor)
+  let booktitle = document.createElement("p");
+  booktitle.textContent = book.title;
+  booktitle.classList.add("title");
+  bookCard.appendChild(booktitle);
 
-    let bookPages = document.createElement('p');
-    bookPages.textContent = book.pages;
-    bookPages.classList.add("page");
-    bookCard.appendChild(bookPages)
+  let bookAuthor = document.createElement("p");
+  bookAuthor.textContent = book.author;
+  bookAuthor.classList.add("author");
+  bookCard.appendChild(bookAuthor);
 
-    let bookRead = document.createElement('p');
-    bookRead.textContent = book.read;
-    bookRead.classList.add("isCompleted");
-    bookCard.appendChild(bookRead)
-    
+  let bookPages = document.createElement("p");
+  bookPages.textContent = book.pages;
+  bookPages.classList.add("page");
+  bookCard.appendChild(bookPages);
 
-    });
+  let bookRead = document.createElement("p");
+  bookRead.textContent = book.read;
+  bookRead.classList.add("read");
+  bookCard.appendChild(bookRead);
+});
 
+addBookButton.addEventListener("click", () => {
+  popup.style.display = "block";
+});
+
+close.addEventListener("click", () => {
+  popup.style.display = "none";
+})
+
+
+
+//add books to array from the form input field
